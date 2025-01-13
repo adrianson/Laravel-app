88abed4eca97c67862fda4fa10fced5e403abc2f
adrianson <adrianson@gmail.com>
2025-01-13 21:41:00 +0200

    Team route

--- web.php
+++ web.php
@@ -10,3 +10,13 @@ Route::get('/portfolio', function () {
     return view('portfolio');
 })->name("portfolio");
 
+Route::get('/team', function () {
+    $team = array(
+        array("name" => "John Johnson", "role" => "CEO", "email" => "j@j.com"),
+        array("name" => "Peter Peterson", "role" => "CFO", "email" => "p@j.com"),
+        array("name" => "Eric Ericson", "role" => "CTO", "email" => "e@j.com"),
+    );
+
+    return view('team')
+        ->with("team", $team);
+})->name("team");
